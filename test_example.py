55e2f72bd19c0544b55290f23e0bae09c600ab47
Sergey Cheparev <s.cheparev@s-v.de>
2014-07-14 15:45:57 +0200

    Added example test

---
+++ test_example.py
@@ -0,0 +1,29 @@
+import pytest
+from tools import simulate
+
+from simulator_fixture import simulator
+
+@pytest.mark.parametrize("params", [{}])
+@simulate
+def test_example(simulator):
+
+    # widgets are selected with xpath
+    simulator.assert_exists("/RootWidget")
+    simulator.assert_count("//RootWidget//Button", 3)
+
+    # deep tree goes reversed through the tree
+    simulator.assert_text("//RootWidget//Button[1]", "Third")
+    simulator.assert_text("//RootWidget//Button[2]", "Second")
+    simulator.assert_text("//RootWidget//Button[3]", "First")
+
+    simulator.assert_text("//RootWidget//StatusLabel", "Status")
+
+    simulator.tap("//RootWidget//Button[3]")
+    simulator.assert_text("//RootWidget//StatusLabel", "First pressed")
+
+    simulator.tap("//RootWidget//Button[2]")
+    simulator.assert_text("//RootWidget//StatusLabel", "Second pressed")
+
+    simulator.assert_disabled("//RootWidget//Button[1]")
+
+    simulator.execute()
